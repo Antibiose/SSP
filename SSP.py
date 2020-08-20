8b5bbb0f2aa793d555f773de70c1ee59eaf1971e
test <nsiddiqui@fastbill.com>
2020-08-20 09:27:01 +0200

    restart of the thing

--- SSP.py
+++ SSP.py
@@ -4,3 +4,15 @@ print("Willkommmen zu Schrere, Stein, Papier - muhahaha")
 spieler1 = input("Schere, Stein oder Papier?")
 
 computer = ("Schere", "Stein", "Papier")
+
+if spieler1 == random.choice(computer):
+    print("Unentschieden!!")
+else:
+    print("Test")
+
+
+def ComputerSSP(computer):
+    computer_pick = random.choice(computer)
+    print(computer_pick)
+
+ComputerSSP(computer)
