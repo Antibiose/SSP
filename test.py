12da9223a0a0d668f601c42cd3ca13801a40d15d
test <nsiddiqui@fastbill.com>
2020-08-21 17:14:57 +0200

    trying out tkinter to build a GUI

--- test.py
+++ test.py
@@ -1,13 +1,73 @@
+import tkinter
+
+top = tkinter.Tk()
+# Code to add widgets will go here...
 import random
+import time
 
-player_pick = "Stein"
-pick_list = ["Stein", "Schere", "Papier"]
 
-a = random.choice(pick_list)
 
-if player_pick == a:
-    print(a)
-    print("Unentschieden")
+picks = ["Stein", "Schere","Papier"]
+player_entry = ["Stein", "Schere","Papier"]
+
+
+def computer_pick_print(computer_picks):
+    print("Der Computer wählt "+ (computer_picks)); time.sleep(0.5)
+
+def player_loses():
+    time.sleep(0.5)
+    print("Du verlierst! :(")
+
+def player_wins():
+    time.sleep(0.5)
+    print("Du gewinnst! :)")
+
+#just for drama purposes - actual ppl do say that when playing
+def game_drama():
+    print("SCHERE!"); time.sleep(0.5)
+    print("STEIN!"); time.sleep(0.5)
+    print("PAPIER!"); time.sleep(0.1)
+
+    
+
+print("Schere, Stein, Papier Action - whoop!"); time.sleep(1)
+player_picks = ""
+while player_picks not in player_entry:
+    player_picks = input("Schere, Stein oder Papier? - Bitte großschreiben: ") 
+
+game_drama()
+
+
+print("Du hast " + player_picks + " gewählt.") 
+computer_picks = random.choice(picks)
+
+
+# computer/player draw
+if player_picks == computer_picks:
+    computer_pick_print(computer_picks); time.sleep(0.5)
+    print("Unentschieden!")
+
+# computer chooses rock
+elif computer_picks == "Stein":
+    computer_pick_print(computer_picks)                                                    
+    if player_picks == "Schere" or player_picks == "schere":
+        player_loses()
+    else:
+        player_wins()
+
+#computer chooses scissors
+elif computer_picks == "Schere":   
+    computer_pick_print(computer_picks)                                                 
+    if player_picks == "Papier" or player_picks == "papier":
+        player_loses()
+    else:
+        player_wins()
+
+#computer chooses Paper
 else:
-    print("negativtest")
-    print(a)
+    if player_picks == "Stein" or player_picks == "stein":
+        computer_pick_print(computer_picks)    
+        player_loses()
+    else:
+        player_wins()                                            
+top.mainloop()
