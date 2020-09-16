81efad5748e11c070a7ebe7af00d2f66ec44fd39
test <nsiddiqui@fastbill.com>
2020-09-16 18:48:38 +0200

    -

--- test.py
+++ test.py
@@ -1,8 +1,23 @@
 import tkinter as tk
+from time import sleep as sleep
 
 window = tk.Tk()
 
-label = tk.Label(text = "DAS IST EIN TEST")
+welcome_message = tk.Label(text = "SCHERE - STEIN - PAPIER")
+sleep = 0.5
+second_message = tk.Label(text = "Hype.")
 
-label.pack
+
+schere = tk.Button(text = "Schere")
+schere.grid(row = 0, column = 0)
+
+stein = tk.Button(text = "Stein")
+stein.grid(row = 0, column = 1)
+
+papier = tk.Button(text = "Papier")
+papier.grid(row = 0, column = 2)
+
+
+
+
 window.mainloop()
